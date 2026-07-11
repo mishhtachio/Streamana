a2a4520dcd6cf1e2868db9c02fa5c6ac6fabc77f
mishhtachio <mishel.muhammed@gmail.com>
2026-07-11 13:06:03 +0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 const ALLOWED_ORIGINS = process.env.CLIENT_ORIGIN
   ? process.env.CLIENT_ORIGIN.split(",")
-  : ["http://localhost:5173", "http://localhost:3000", "https://streamama.onrender.com"];
+  : ["https://streamama.onrender.com"];
 
 app.use(cors({ origin: ALLOWED_ORIGINS }));
 app.use(helmet());
